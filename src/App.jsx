b058031d2e6f63a535d8dc1c1bfd9571c22d97e3
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-07-27 17:28:40 -0400

    matching id on click to id in state and changging boolean

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import {nanoid} from "nanoid"
 
 function App() {
 
-  const holdDice = (id) => {
-    console.log(id)
-  }
-
-
   const allNewDice = () => {
     const numArray = [];
     for(let i = 1; i <= 10; i++){
@@ -22,6 +17,27 @@ function App() {
   const dieComp = diceNum.map( (data) => {
       return <Die value={data.value} key={data.id} held={data.isHeld} id={data.id} hold={holdDice} />
   })
+
+    /**
+ * Challenge: Update the `holdDice` function to flip
+ * the `isHeld` property on the object in the array
+ * that was clicked, based on the `id` prop passed
+ * into the function.
+ * 
+ * Hint: as usual, there's > 1 way to accomplish this.
+ * I'll be using `dice.map()` and checking for the `id`
+ * of the die to determine which one to flip `isHeld` on,
+ * but you can do whichever way makes the most sense to you.
+ */
+
+    const holdDice = (id) => {
+      setDiceNum(prevData => prevData.map( diceData => {
+        if(diceData.id === id){
+          return {...diceData, isHeld: !diceData.isHeld}
+        }else return diceData
+      })
+  
+    }
 
   const rollDice = () => {
     setDiceNum(allNewDice())
